2bcf9b21f18cee4e395925b27e814146fcae9c41
Ijlal Ishaq <ijlalishaq2019@gmail.com>
2022-07-16 14:37:17 +0500

    structured

--- BorrowableNFT.sol
+++ BorrowableNFT.sol
@@ -1,67 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./ERC721Borrowable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BorrowableNFT is ERC721Enumerable, Ownable {
+contract BorrowableNFT is ERC721Borrowable {
     using Counters for Counters.Counter;
-    Counters.Counter private tokenId;
-    
-    struct BorrowDetail {
-        address borrower;
-        uint timestamp;
+    Counters.Counter private _tokenId;
+
+    constructor() ERC721Borrowable("BorrowableNFT", "B-NFT"){}
+
+    function mint(address _to) external {
+        _tokenId.increment();
+        _safeMint(_to, _tokenId.current());
     }
 
-    mapping(uint=>BorrowDetail) private borrowDetails;
-
-    constructor() ERC721("BorrowableNFT", "B-NFT") {
-        tokenId.increment();
+    function totalSuppy() public view returns(uint256){
+        return _tokenId.current();
     }
 
-    function lendingToken(uint _tokenId , address _borrower , uint _endTime) external {
-        require(_isApprovedOrOwner(msg.sender,_tokenId) == true , "You don't have the rights.");
-        require(_endTime > block.timestamp , "The end time has passed.");
-        require(isBorrowed(_tokenId) == false , "Token is already borrowed.");
-
-        BorrowDetail memory obj = BorrowDetail(_borrower,_endTime);
-        borrowDetails[_tokenId] = obj;
-
-    }
-
-    function isBorrowed(uint _tokenId) public view returns (bool){
-        if(borrowDetails[_tokenId].timestamp >= block.timestamp){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    function mint() external {
-        _safeMint(msg.sender , tokenId.current());
-    }
-
-    function safeTransferFrom(address from , address to , uint256 _tokenId) public virtual override {
-        require(isBorrowed(_tokenId) == false , "Token is borrowed.");
-        super.safeTransferFrom(from,to,_tokenId);
-    }
-
-    function safeTransferFrom(address from , address to , uint256 _tokenId , bytes memory _data) public virtual override {
-        require(isBorrowed(_tokenId) == false , "Token is borrowed.");
-        super.safeTransferFrom(from,to,_tokenId,_data);
-    }
-
-    function transferFrom(address from , address to , uint256 _tokenId) public virtual override {
-        require(isBorrowed(_tokenId) == false , "Token is borrowed.");
-        super.transferFrom(from,to,_tokenId);
-    }
-
-    function ownerOf(uint256 _tokenId) public view virtual override returns (address) {
-        if(isBorrowed(_tokenId)){
-            return borrowDetails[_tokenId].borrower;
-        }else{
-            return super.ownerOf(_tokenId);
-        }
-    }
-    
 }
+
